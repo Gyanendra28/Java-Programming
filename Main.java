ce6b9766497c626186fcea90c225fcefc3555ad2
Gyanendra28 <101047992+Gyanendra28@users.noreply.github.com>
2025-01-04 17:02:15 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,27 @@
-package Transport;
+package Polymorphism;
 
 public class Main {
     public static void main(String[] args) {
-        Car c = new Car("Maruti", "800", 4, 5, "Automatic");
-        c.startEngine();
-        c.startAC();
-        c.stopEngine();
+        // Static Polymorphism
+        // Calculator c = new Calculator();
+        // System.out.println(c.add(2, 3));
+        // System.out.println(c.add(2, 3, 4));
+        // System.out.println(c.add(2, 3, 4, 5.6));
 
-        MotorCycle m = new MotorCycle("Splendor", "Xline", 2, "U", "Soft");
-        m.startEngine();
-        m.wheelie();
-        m.stopEngine();
+        // Dynamic Polymorphism
+        // Circle c = new Circle();
+        // doDrawing(c);
+
+        // Rectangle r = new Rectangle();
+        // doDrawing(r);
+
+        Circle c1 = new Circle();
+        doDrawing(c1);
+    }
+
+    public static void doDrawing(Shape s) {
+        s.draw();
+        Circle c1 = (Circle)s; // Downcasting
+        c1.draw();
     }
 }
