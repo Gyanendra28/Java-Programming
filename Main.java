aee25fc88bd30759834e9d82e7f289ce226e4e80
Gyanendra28 <101047992+Gyanendra28@users.noreply.github.com>
2025-01-05 14:22:05 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,27 +1,99 @@
-package Polymorphism;
+package AbstractDesign;
+
+// abstract class Bird {
+//     abstract void fly();
+//     abstract void eat();
+
+//     public void sleep() {
+//         System.out.println("Bird is sleeping");
+//     }
+// }
+
+// class Nightingale extends Bird {
+//     @Override
+//     void fly() {
+//         System.out.println("Nightingale is flying");
+//     }
+
+//     @Override
+//     void eat() {
+//         System.out.println("Nightingale is eating");
+//     }
+// }
+
+// class Sparrow extends Bird {
+//     @Override
+//     void fly() {
+//         System.out.println("Sparrow is flying");
+//     }
+
+//     @Override
+//     void eat() {
+//         System.out.println("Sparrow is eating");
+//     }
+// }
 
 public class Main {
+    public static void doBirdStuff(Bird b) {
+        b.fly();
+        b.eat();
+        b.sleep();
+    }
     public static void main(String[] args) {
-        // Static Polymorphism
-        // Calculator c = new Calculator();
-        // System.out.println(c.add(2, 3));
-        // System.out.println(c.add(2, 3, 4));
-        // System.out.println(c.add(2, 3, 4, 5.6));
+        doBirdStuff(new Nightingale());
+        doBirdStuff(new Sparrow());
 
-        // Dynamic Polymorphism
-        // Circle c = new Circle();
-        // doDrawing(c);
+        // Bird b = new Nightingale();
+        // b.fly();
+        // b.eat();
 
-        // Rectangle r = new Rectangle();
-        // doDrawing(r);
+        // b = new Sparrow();
+        // b.eat();
+        // b.fly();
 
-        Circle c1 = new Circle();
-        doDrawing(c1);
+        // b.sleep();
+    }
+}
+
+// Interface
+interface Bird {
+    void fly(); // Public
+    void eat();
+
+    default void sleep() {
+        System.out.println("Bird is sleeping");
+    }
+}
+
+interface Walk {
+    void walk();
+}
+
+class Nightingale implements Bird, Walk {
+    @Override
+    public void fly() {
+        System.out.println("Nightingale is flying");
     }
 
-    public static void doDrawing(Shape s) {
-        s.draw();
-        Circle c1 = (Circle)s; // Downcasting
-        c1.draw();
+    @Override
+    public void eat() {
+        System.out.println("Nightingale is eating");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("Nightingale is walking");
+    }
+}
+
+class Sparrow implements Bird {
+    @Override
+    public void fly() {
+        System.out.println("Sparrow is flying");
+    }
+    
+    @Override
+    public void eat() {
+        System.out.println("Sparrow is eating");
     }
 }
